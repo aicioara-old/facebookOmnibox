dece0f514fee6153c35cac01b2098d1b0f1a4433
aicioara <primulandrei@yahoo.com>
2014-05-26 18:05:10 +0100

    Fixed a fallback

--- openMenu.js
+++ openMenu.js
@@ -1,10 +1,13 @@
-console.log("Hello WOrl");
+window.onload = function() {
+	document.getElementsByClassName('_586i')[0].focus();
+}
 
+
+/**
+ * Fallback in case the onload fails. This should be fixed
+ */
 setTimeout(function() {
-	console.log("Loaded");
-	document.getElementsByClassName('_586i')[0].focus()
+	document.getElementsByClassName('_586i')[0].focus();
 }, 1000);
 
-window.onload = function() {
-}
 
